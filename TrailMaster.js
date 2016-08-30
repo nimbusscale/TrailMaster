e7812326fd0ab7635fc555fdf24bea08946faf1f
NimbusScale <joe@nimbusscale.com>
2016-08-30 10:46:02 -0700

    inital

---
+++ TrailMaster.js
@@ -0,0 +1,14 @@
+var TrailMaster = function () {
+    const version = '0.0.1';
+    var settings = {
+        tokenCharMgrEnabled: true,
+      };
+    log(`-=> TrailMaster v${version} <=-`);
+  };
+
+on('ready', function () {
+    'use strict';
+
+    var TM = new TrailMaster();
+  });
+
