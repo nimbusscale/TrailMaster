16e61a540bf03c4ab2321b6bcb48fc8613288ea5
NimbusScale <joe@nimbusscale.com>
2016-08-30 11:39:27 -0700

    TCM skeleton as function

--- TrailMaster.js
+++ TrailMaster.js
@@ -1,14 +1,25 @@
 var TrailMaster = function () {
     const version = '0.0.1';
-    var settings = {
+    this.settings = {
         tokenCharMgrEnabled: true,
-      };
+    };
     log(`-=> TrailMaster v${version} <=-`);
-  };
+};
+
+TrailMaster.prototype.TokenCharMgr = function () {
+    const version = '0.0.1';
+    this.settings = {
+        tokenCharMgrEnabled: true,
+    };
+    log(`-=> TrailMaster - TokenCharMgr v${version} <=-`);
+};
 
 on('ready', function () {
     'use strict';
 
     var TM = new TrailMaster();
-  });
+    if (TM.settings.tokenCharMgrEnabled) {
+        var TCM = new TM.TokenCharMgr();
+    };
+});
 
