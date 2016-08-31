799f3610ae9c9aedd707e1b5c16d2012b55af225
NimbusScale <joe@nimbusscale.com>
2016-08-31 00:57:08 -0700

    working Turn Announcer

--- TrailMaster.js
+++ TrailMaster.js
@@ -47,7 +47,7 @@ TrailMaster.prototype.RoundTurnMgr = function () {
   const version = '0.0.1';
   return {
     RegisterEventHandlers: function () {
-      on('change:campaign:turnorder', function(obj, prev) {
+      on('change:campaign:turnorder', function (obj, prev) {
         RTM.AnnounceCurrentTurn();
       });
 
@@ -105,16 +105,34 @@ TrailMaster.prototype.RoundTurnMgr = function () {
     },
     AnnounceCurrentTurn: function () {
       var turnOrder = RTM.TurnOrder.Get();
-      var currentToken = getObj("graphic", turnOrder[0].id);
+      var currentToken = getObj('graphic', turnOrder[0].id);
       if (currentToken.get('layer') === 'gmlayer') {
         return;
       }
 
+      const tokenSize = 70;
       var tokenType = TCM.TokenType(currentToken);
       var tokenIMG = currentToken.get('imgsrc');
       var tokenName = currentToken.get('showplayers_name') ? currentToken.get('name') : 'NPC';
-      var nameString = `<span style="font-size: 115%; font-weight:bold; text-decoration: underline"><a href="https://journal.roll20.net/character/${currentToken.get('represents')}">${tokenName}</a></span>`;
-      log(nameString);
+      var nameString = `<span style='font-size: 115%; font-weight:bold; text-decoration: underline;'><a href='https://journal.roll20.net/character/${currentToken.get('represents')}'>${tokenName}</a></span>`;
+
+      if (tokenType === 'player') {
+        var bgColor = '#efe';
+        var char = getObj('character', currentToken.get('represents'));
+        var currentHP = getAttrByName(char.id, 'HP');
+        var tempHP = getAttrByName(char.id, 'HP-temp');
+        var totalHP = Number(currentHP) + Number(tempHP);
+        var speed = getAttrByName(char.id, 'speed-modified');
+        var maxHP = getAttrByName(char.id, 'HP', 'max');
+        var nonLeathal = getAttrByName(char.id, 'non-lethal-damage');
+        var charInfo = `<span style='font-weight: bold;'>HP: </span>${totalHP}/${maxHP} ${nonLeathal != 0 ? `<span style='font-weight: bold;'>NLD: </span> ${nonLeathal}` : ``} <br> <span style='font-weight: bold;'>Speed: </span> ${speed} ft`;
+      } else {
+        var bgColor = '#eef';
+      }
+
+      var imgSrcTag = `<img src='${tokenIMG}' style='float:right; width:${Math.round(tokenSize * .8)}px; height:${Math.round(tokenSize * .8)}px; padding: 0px 2px;' />`;
+      var announceBox = `<div style='border: 3px solid #808080; background-color: ${bgColor}; padding: 1px 1px;'> <div style='text-align: left; margin: 5px 5px; position: relative; vertical-align: text-top;'> ${imgSrcTag} ${nameString} <br> ${charInfo ? charInfo : ''} <div style="clear:both;"></div></div></div>`;
+      sendChat('', `/direct ${announceBox}`);
     },
   };
 };
@@ -126,11 +144,10 @@ var RTM = new TM.RoundTurnMgr();
 on('ready', function () {
   'use strict';
   if (TM.settings.tokenCharMgrEnabled) {
-    log('enabled')
-  };
+  }
 
   if (TM.settings.roundTurnMgrEnabled) {
-    RTM.RegisterEventHandlers()
-  };
+    RTM.RegisterEventHandlers();
+  }
 
 });
